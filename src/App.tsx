383425e156c0260b963402ec238a4b5a396154e7
Oswaldodlg <oswaldodlg25@gmail.com>
2022-05-26 22:33:44 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import * as React from "react";
-import { Routes, Route, Navigate} from "react-router-dom";
+import { Routes, Route} from "react-router-dom";
 import "./App.css";
 import Login from "./pages/Login/Login";
 import News from "./pages/News/News";
